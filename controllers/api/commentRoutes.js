280a605ab764021d574a430b8946c2c51752acdb
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-08-24 14:58:20 -0400

    Changed commentId to UserId in the commentRoutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,17 +3,17 @@ const { Comment, } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const postData = Post.findOne({
-      include: [
-        {
-          model: 'post',
-          foreignKey: 'id'
-        }
-      ]
-    })
+    // const postData = Post.findOne({
+    //   include: [
+    //     {
+    //       model: 'post',
+    //       foreignKey: 'id'
+    //     }
+    //   ]
+    // })
     const newComment = await Comment.create({
       ...req.body,
-      commentId: req.session.commentId,
+      userId: req.session.userId,
     });
 
     res.status(200).json(newComment);
