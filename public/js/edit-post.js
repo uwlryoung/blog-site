7c78dd0453051a2db2ffbb99c61b8292a15439d4
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2024-02-07 17:43:27 -0500

    Updated the edit-post.js and postRoutes.js so now posts can successfully be edited

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,16 +1,16 @@
 async function editFormHandler(event) {
   event.preventDefault();
-  const postTitle = document.querySelector('#post-title').value;
+  const title = document.querySelector('#post-title').value;
   const postBody = document.querySelector('#post-body').value;
 
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 2
   ];
 
-  const response = await fetch(`/api/post/${id}`, {
+  const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-      postTitle,
+      title,
       postBody
     }),
     headers: {
